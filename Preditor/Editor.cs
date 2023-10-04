db702a8d7608e37d8f61d6d8291dee545c075a4f
Justin Behanna <j.behanna@gmail.com>
2023-10-04 00:09:34 -0400

    messing with registered options

--- Preditor/Editor.cs
+++ Preditor/Editor.cs
@@ -8,6 +8,7 @@ namespace Preditor
     {
         private Starbrite _engine;
         private bool _isRunning = false;
+        private bool _showDemoWindow = false;
 
         public bool IsRunning => _isRunning;
 
@@ -20,19 +21,54 @@ namespace Preditor
         public void Draw(GameTime gameTime)
         {
             if (ImGui.Button("Power " + (_isRunning ? "Off": "On"))) Toggle("isRunning");
-
+            if (ImGui.Button("Demo " + (_showDemoWindow ? "Off" : "On"))) Toggle("showDemo");
             if (ImGui.Button("List Config Files")) _engine.ListConfigFiles(); // get file list
 
             if (_engine.ConfigFileArray.Count() > 0)
             {
-                if (ImGui.Button("Scan Config Files")) _engine.ScanConfigFiles(); // scan file content
+                if (ImGui.Button("Scan " + _engine.ConfigFileArray.Count().ToString() + " Config Files")) _engine.ScanConfigFiles(); // scan file content
             }
 
             ImGui.Text(string.Format("Application average {0:F3} ms/frame ({1:F1} FPS)", 1000f / ImGui.GetIO().Framerate, ImGui.GetIO().Framerate));
 
-            ImGui.Text(_engine.ConfigFileArray.Count().ToString());
+
+            if (_showDemoWindow)
+            {
+                ImGui.ShowDemoWindow();
+            }
+
+            ShowOptionsTable();
         }
 
+        ///// IMGUI Functions - call from Draw()
+        public void ShowOptionsTable()
+        {
+
+            ImGui.SeparatorText("Options");
+            foreach (var option in _engine.Options) 
+            { 
+                ImGui.Text(option.Name + ": "); 
+                ImGui.SameLine();
+                ImGui.Text(GetOptionValue(option));
+            }
+        }
+
+        public string GetOptionValue(StarbriteOption _option)
+        {
+            switch (_option.Type)
+            {
+                case "string":
+                    var os = (StarbriteOptionString)_option;
+                    return os.Value;
+                case "int":
+                    var oi = (StarbriteOptionInt)_option;
+                    return oi.Value.ToString();
+            }
+
+            return "ERROR: GetOptionValue";
+        }
+
+        // get rid of this shit
         public bool Toggle(string setting)
         {
             switch (setting)
@@ -40,6 +76,9 @@ namespace Preditor
                 case "isRunning":
                     _isRunning = !_isRunning;
                     return _isRunning;
+                case "showDemo":
+                    _showDemoWindow = !_showDemoWindow;
+                    return _showDemoWindow;
             }
 
             return false;
--- Preditor/Starbrite.cs
+++ Preditor/Starbrite.cs
@@ -6,20 +6,60 @@ using Microsoft.Xna.Framework;
 
 namespace Preditor
 {
+    public class StarbriteOption
+    {
+        public string Type;
+        public string Name;
+        public string Description;
+
+        public StarbriteOption(string _type, string _name, string? _description = "")
+        {
+            Type = _type;
+            Name = _name;
+            Description = _description;
+        }
+
+    }
+
+    public class StarbriteOptionString : StarbriteOption
+    {
+        public string Value;
+        public StarbriteOptionString(string _name, string _value, string? _description) : base("string", _name, _description) 
+        {
+            Value = _value;
+        }
+    }
+
+    public class StarbriteOptionInt : StarbriteOption
+    {
+        public int Value;
+        public StarbriteOptionInt(string _name, int _value, string? _description) : base("int", _name, _description)
+        {
+            Value = _value;
+        }
+    }
+
     public class Starbrite
     {
         private Color _colorBackground = Color.Fuchsia;
-        private string _homeDirectory = "."; // AppContext.BaseDirectory works but relative is better
+        private string _homeDirectory = ".";
         private string _configFilter = "*.ses";
         private IEnumerable<string> _configFiles;
         public Color ColorBackground => _colorBackground;
         public string HomeDirectory => _homeDirectory;
         public IEnumerable<string> ConfigFileArray => _configFiles;
 
+        private List<StarbriteOption> _options;
+        public List<StarbriteOption> Options => _options;
+
 
         public Starbrite() 
         { 
             _configFiles = new string[] { };
+
+            _options = new List<StarbriteOption>();
+            _options.Add(new StarbriteOptionString("Server", "irc.uncle.moe", "The main server to connect to."));
+            _options.Add(new StarbriteOptionInt("Port", 2223, "The server port to connect to."));
         }
 
         public int ListConfigFiles()
